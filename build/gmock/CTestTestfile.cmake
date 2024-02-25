4715e39f4dc3fb40c4ecdab54bbd79c3b238aee2
BCaran <branimir.caran@gmail.com>
2024-02-25 08:11:52 +0100

    deleted build file from vscode

--- build/gmock/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/ros/humble/src/gmock_vendor
-# Build directory: /home/branimir/wcr_ws/src/four_wheel_steering_controller/build/gmock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/ros/humble/src/gtest_vendor
-# Build directory: /home/branimir/wcr_ws/src/four_wheel_steering_controller/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
